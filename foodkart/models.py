26808c445cddb4d6cf57d641b6718dc7fbf157ac
Yashbontala <cse190001067@iiti.ac.in>
2021-05-08 22:56:41 +0530

    menumodel

--- foodkart/models.py
+++ foodkart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    restaurant_id=models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    food_name=models.CharField(max_legth=30)
+    description=models.CharField(max_length=350)
+    rating=models.IntegerField()
+    food_image=models.ImageField(upload_to='images/')
+    veg=models.BooleanField(default=True)
+    price=models.IntegerField()
