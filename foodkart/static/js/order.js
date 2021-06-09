2c1747f67ed5b1df4494deb4cf7a6e70705387a0
Yashbontala <cse190001067@iiti.ac.in>
2021-06-10 04:44:14 +0530

    firebase

--- foodkart/static/js/order.js
+++ foodkart/static/js/order.js
@@ -1,14 +1,7 @@
-var firebaseConfig = {
-	apiKey: "AIzaSyDfGQmsSdjAM77R7g4rkCa_d7FXsaPcl_I",
-	authDomain: "test2-374dc.firebaseapp.com",
-	databaseURL: "https://test2-374dc-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "test2-374dc",
-	storageBucket: "test2-374dc.appspot.com",
-	messagingSenderId: "900145299510",
-	appId: "1:900145299510:web:baed02e2b01bd767225897"
-};
-firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+// import { firebase } from './firebase.js';
+
+
+
 function writeOrder() {
 	var info = JSON.parse(document.getElementById('info').textContent);
 	var rests = JSON.parse(document.getElementById('rests').textContent);
