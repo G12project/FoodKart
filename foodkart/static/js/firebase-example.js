e6b914947c14a004130dc84bb227b17dd0d7a7f0
Jyotishna Baishya <73800133+JyotishnaBaishya@users.noreply.github.com>
2021-06-17 15:54:28 +0530

    Update and rename firebase.js to firebase-example.js

---
+++ foodkart/static/js/firebase-example.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+	apiKey: //your apikey,
+	authDomain: //your authdomain",
+	databaseURL: //your url,
+	projectId: //your id,
+	storageBucket: //your bucket,
+	messagingSenderId: //your value,
+	appId: //your appid
+};
+firebase.initializeApp(firebaseConfig);
